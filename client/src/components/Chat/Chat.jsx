a195774078ddce8467e9bf9f08ab381b1bc0efd6
Lm09 <123.moharana@gmail.com>
2024-02-23 18:34:25 +0530

    Added join room frontend

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
 const Chat = () => {
   return <h1>Chat</h1>;
--- server/index.js
+++ server/index.js
@@ -3,11 +3,22 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
+const router = require("./router");
 
 const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("User connected!");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
